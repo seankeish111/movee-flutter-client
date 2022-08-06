032ddf3984897b7a46eec772fcc08ed346e4f48a
seankeish111 <shankesh.s@outlook.com>
2022-08-07 05:22:41 +1000

    Navigation added with stateful widgets

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,93 +2,73 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 
-
-
-
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({
     Key? key,
     required this.user,
   }) : super(key: key);
-
   final User user;
 
   @override
-  Widget build(BuildContext context) {
-    print(user);
-    var size = MediaQuery.of(context).size;
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Movee'),backgroundColor: Colors.redAccent,
-        ),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(50.0),
-                  child: Text(
-                    user.email!,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
+  State<HomeScreen> createState() => _HomeScreenState();
 
-              Column(
-
-                children: [
-                  Padding(
-
-                    padding: const EdgeInsets.all(50.0),
-                    child: Center(child: const SignOutButton()),
-                  ),
-                ],
-              ),
-              ],
-            ),
-
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          backgroundColor: Colors.redAccent,
-
-        ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.connect_without_contact),
-            label: 'Home',
-            backgroundColor: Colors.redAccent,
-
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.assessment),
-            label: 'text',
-            backgroundColor: Colors.redAccent,
-
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_box),
-            label: 'Home',
-            backgroundColor: Colors.redAccent,
-
-          ),
-
-
-
-        ],
-
-
-
-      ),
-    );
-
-
-  }
 }
 
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Movee'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+
+            Column(
+              children: const[
+                Padding(
+                  padding:  EdgeInsets.all(50.0),
+                  child: Center(child:  SignOutButton()),
+                )
+              ],
+            )
+          ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.redAccent,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        selectedFontSize: 16,
+
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
+        items: const [
+           BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+              backgroundColor: Colors.white,
+
+
+          ),
+
+           BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Menu",
+            backgroundColor: Colors.black,
+          ),
+
+           BottomNavigationBarItem(
+            icon:  Icon(Icons.home),
+            label: "Account",
+            backgroundColor: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
