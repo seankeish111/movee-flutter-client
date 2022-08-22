172854f9ce03141516642fc052581c18aac58c68
seankeish111 <seankeish@Seans-MBP.modem>
2022-08-23 00:27:26 +1000

    parse server connected with widgets working condition

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'auth_gate.dart';
+import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:movee/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 
 
@@ -9,6 +11,14 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final keyApplicationId = '2PU1weaeX52ATYyArgdEQYO6Epiy0jSMeZTYrNvq';
+  final keyClientKey = 'lBA973lKk2rpnUXCZ2vrtw3EZvwQV72g44UZuEtX';
+  final keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, debug: true);
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,19 +6,27 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import connectivity_plus_macos
 import desktop_webview_auth
 import firebase_auth
 import firebase_core
 import firebase_database
 import firebase_storage
+import package_info_plus_macos
+import path_provider_macos
+import shared_preferences_macos
 import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
 }
