4f6dd8caa6aa623a31b442782a7026e35cc79ba4
seankeish111 <seankeish@Seans-MacBook-Pro.local>
2022-08-24 18:26:40 +1000

    int field added not wokring with number inputs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:math';
-import 'package:movee/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
@@ -16,6 +15,7 @@ class home_page extends StatefulWidget {
 class _home_pageState extends State<home_page> {
   final FirstNameController = TextEditingController();
   final SecondNameController = TextEditingController();
+  final NoofBedroomsController = TextEditingController();
 
 
 
@@ -39,14 +39,29 @@ class _home_pageState extends State<home_page> {
       return;
     }
 
+    if (NoofBedroomsController.text.trim().isEmpty) {
+
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Please enter the number of bedrooms worth in your property"),
+        duration: Duration(seconds: 2),
+      ));
+      return;
+    }
+
+
+
+
+
     await saveFirstName(FirstNameController.text);
     await saveSecondName(SecondNameController.text);
+    await saveSecondName(NoofBedroomsController.text);
 
 
 
     setState(() {
       FirstNameController.clear();
       SecondNameController.clear();
+      NoofBedroomsController.clear();
 
     });
   }
@@ -56,7 +71,9 @@ class _home_pageState extends State<home_page> {
 
 
   @override
+
   Widget build(BuildContext context) {
+
     return Scaffold(
         appBar: AppBar(
         title: Text("Enquiry Form"),
@@ -64,95 +81,141 @@ class _home_pageState extends State<home_page> {
     centerTitle: true,
     ),
         body: Column(
+
             children: <Widget>[
+
         Column(
           children: [
-            Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              verticalDirection: VerticalDirection.down,
-              children: [
-                SizedBox(
-                  height: 300,
-                  width: 300,
-                  child: Column(
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Center(
+                child: Column(
 
 
-                    children: <Widget>[
-                      Expanded(
-                        child: SizedBox(
-                          child: Container(
-                            height: 400,
-                            width: 400,
-                            child: TextField(
-                              autocorrect: true,
-                              textCapitalization: TextCapitalization.sentences,
-                              controller: FirstNameController,
-                              decoration: InputDecoration(
-                                  labelText: "First name",
-                                  labelStyle: TextStyle(color: Colors.redAccent
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  verticalDirection: VerticalDirection.down,
+                  children: [
+                    SizedBox(
+                      height: 300,
+                      width: 300,
+
+
+
+
+
+
+
+                child: Column(
+
+
+
+                        children: <Widget>[
+                          Expanded(
+                            child: SizedBox(
+                              width: 300,
+                              height: 300,
+                              child: Container(
+                                child: TextField(
+                                  autocorrect: true,
+                                  textCapitalization: TextCapitalization.sentences,
+                                  controller: FirstNameController,
+                                  decoration: InputDecoration(
+                                      labelText: "First name",
+                                      labelStyle: TextStyle(color: Colors.redAccent
+                                      ),
                                   ),
+                                ),
                               ),
                             ),
                           ),
 
-
-                        ),
-
-
-                      ),
-
-                      Expanded(
-                        child: SizedBox(
-                          child: Container(
-                            height: 400,
-                            width: 400,
-                            child: TextField(
-                              autocorrect: true,
-                              textCapitalization: TextCapitalization.sentences,
-                              controller: SecondNameController,
-                              decoration: InputDecoration(
-                                labelText: "Second name",
-                                labelStyle: TextStyle(color: Colors.redAccent
+                          Expanded(
+                            child: SizedBox(
+                              width: 300,
+                              height: 300,
+                              child: Container(
+                                child: TextField(
+                                  autocorrect: true,
+                                  textCapitalization: TextCapitalization.sentences,
+                                  controller: SecondNameController,
+                                  decoration: InputDecoration(
+                                    labelText: "Second name",
+                                    labelStyle: TextStyle(color: Colors.redAccent
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
                           ),
 
 
-                        ),
+                          Expanded(
+                            child: SizedBox(
+                              width: 300,
+                              height: 300,
 
 
+                              child: Container(
+
+                                child: TextField(
+                                  autocorrect: true,
+                                  textCapitalization: TextCapitalization.sentences,
+                                  controller: NoofBedroomsController,
+                                  decoration: InputDecoration(
+                                    labelText: "Number of bedrooms",
+                                    labelStyle: TextStyle(color: Colors.redAccent
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+
+                            ),
+
+
+                          ),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+
+                                onPrimary: Colors.white,
+                                primary: Colors.redAccent,),
+                              onPressed: addToDomForm,
+                              child: Text("Submit")),
+
+                          // ElevatedButton(
+                          //     style: ElevatedButton.styleFrom(
+                          //       onPrimary: Colors.white,
+                          //       primary: Colors.redAccent,),
+                          //     onPressed: (){
+                          //       Navigator.push(context, MaterialPageRoute(builder: (context) => testing(items: [],) ));
+                          //     },
+                          //     child: Text("Test")),
+                        ],
                       ),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            onPrimary: Colors.white,
-                            primary: Colors.redAccent,),
-                          onPressed: addToDomForm,
-                          child: Text("Submit")),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            )),
+              ),
+            ),
           ],
         ),
 
@@ -165,9 +228,17 @@ class _home_pageState extends State<home_page> {
 
 
   async {
-    final DomForm = ParseObject('DomesticEnquiry')
+    final DomForm = ParseObject('Enquiry')
+
+
+
+
       ..set('FirstName', FirstNameController.text.trim())
-      ..set('SecondName', SecondNameController.text.trim());
+      ..set('SecondName', SecondNameController.text.trim())
+      ..set('NoofBedrooms',int.parse(NoofBedroomsController.text.trim()));
+
+
+
 
 
 
